0fb173ed04df6ee6cc316d62973b2c68a04c3cb7
ess_ess <sravans2011@gmail.com>
2016-05-29 18:21:39 +0530

    Added openshift information

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+app.listen(server_port, function () {
   console.log('Example app listening on port 3000!');
 });
 
